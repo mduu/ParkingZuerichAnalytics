ea49ed96ddf604abf4951927cbe02e781a014c36
Marc Dürst <marc@marcduerst.com>
2023-10-04 09:05:49 +0200

    Add fetching code

--- src/parkingzuerich-app/app/page.tsx
+++ src/parkingzuerich-app/app/page.tsx
@@ -1,6 +1,30 @@
+'use client'
+
 import Image from 'next/image'
+import useSWR from 'swr'
+
+export interface ParkingAddress {
+  ParkingName: string | undefined;
+  Title: string | undefined;
+  Address: string | undefined;
+  Url: string | undefined;
+}
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 export default function Home() {
+  const { data, error } = useSWR<ParkingAddress[], any>(
+      `https://parkingzuerichanalytics.azurewebsites.net/api/parking?code=dduM8-mNw2DNH65g0eV8SrRoe-5hm92FzkXw4KjHrT2PAzFupfKKqw==`,
+      fetcher
+  )
+  let statusText = 'ok';
+  if (error) statusText = 'Failed to load';
+  if (!data) statusText = 'Loading...';
+
+  console.log('data=' + data)
+  
+  const parkings = data != undefined ? data.map(p => <li key={p.ParkingName}>{p.Title}</li>) : "";
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -29,14 +53,8 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <label>Status: {statusText}</label>
+        <ul>{parkings}</ul>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
