78ed3a2afe9cc7a2cd6a298494a8b56413e855d1
Marc Dürst <marc@marcduerst.com>
2023-10-14 11:41:53 +0200

    Change to JoyUI

--- src/parkingzuerich-app/app/components/ParkingPicker.tsx
+++ src/parkingzuerich-app/app/components/ParkingPicker.tsx
@@ -1,6 +1,14 @@
-export function ParkingPicker()
-{
+import { ParkingAddress } from "@/src/models";
+
+export function ParkingPicker(props: { parkings: ParkingAddress[] }) {
     return (
-        <h3>I am the ParkingPicker</h3>
+        <div>
+            <h3>I am the ParkingPicker</h3>
+            <ul>
+                {props.parkings.map(p => (
+                    <li key={p.parkingName}>{p.title}</li>
+                ))}
+            </ul>
+        </div>
     );
 }
