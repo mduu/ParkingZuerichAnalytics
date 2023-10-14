9cbcdc1ebd997c39ff405829d861c3a7f5db6582
Marc Dürst <marc@marcduerst.com>
2023-10-14 13:54:10 +0200

    Improve ParkingPicker

--- src/parkingzuerich-app/app/components/ParkingPicker.tsx
+++ src/parkingzuerich-app/app/components/ParkingPicker.tsx
@@ -3,9 +3,20 @@ import { ParkingAddress } from "@/src/models";
 import { Select, Option } from "@mui/joy";
 
 export function ParkingPicker(props: { parkings: ParkingAddress[] }) {
+    const handleChange = (
+        event: React.SyntheticEvent | null,
+        newValue: string | null,
+    ) => {
+        alert(`You chose "${newValue}"`);
+    };
+    
     return (
         <Fragment>
-            <Select defaultValue="dog">
+            <Select
+                onChange={handleChange}
+                placeholder="Select a parking"
+                variant="soft">
+                
                 {props.parkings.map(p => (
                     <Option key={p.parkingName} value={p.parkingName}>{p.title}</Option>
                 ))}
